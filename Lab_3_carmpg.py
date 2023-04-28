7812fc7b3903380078961fd0a1d4bf47ac0c67b2
nthng-quan <hoangquan26102002@gmail.com>
2023-04-29 00:43:20 +0700

    3.4

--- Lab_3_carmpg.py
+++ Lab_3_carmpg.py
@@ -5,7 +5,7 @@ Created on Tue Jan 27 21:53:34 2015
 @author: nymph
 """
 
-#%%
+# %%
 #################################### Read the data ############################
 import pandas as pd
 from pandas import DataFrame, Series
@@ -19,12 +19,12 @@ The data structure is able to deal with complex table data whose attributes are 
 Row names, column names in the dataframe can be used to index data.
 '''
 
-data = pd.read_csv("http://archive.ics.uci.edu/ml/machine-learning-databases/auto-mpg/auto-mpg.data-original", delim_whitespace = True, \
- header=None, names = ['mpg', 'cylinders', 'displacement', 'horsepower', 'weight', 'acceleration', 'model', 'origin', 'car_name'])
+data = pd.read_csv("http://archive.ics.uci.edu/ml/machine-learning-databases/auto-mpg/auto-mpg.data-original", delim_whitespace=True,
+                   header=None, names=['mpg', 'cylinders', 'displacement', 'horsepower', 'weight', 'acceleration', 'model', 'origin', 'car_name'])
 
 data['mpg']
 data.mpg
-data.iloc[0,:]
+data.iloc[0, :]
 
 print(data.shape)
 
@@ -37,39 +37,54 @@ data_['type_car'] = data_.car_name.str.split().str.get(0)
 q8 = data_.groupby(['model'])
 q8_data = q8['type_car'].count()
 
+
 def q8_plot():
     ''' Create a canvas with width 10, height 7.5 '''
     fig_obj = plt.figure(figsize=(10, 7.5))
     sns.set_style("whitegrid")
-    sns.lineplot(data =q8_data,legend = 'auto',color= 'orange',marker = 'o').set(title = 'A number of new cars introduced each year',xlabel = 'model',ylabel = 'count')
+    sns.lineplot(data=q8_data, legend='auto', color='orange', marker='o').set(
+        title='A number of new cars introduced each year', xlabel='model', ylabel='count')
 
-    ax = plt.subplot(111)  
+    ax = plt.subplot(111)
     ax.spines['right'].set_visible(False)
     ax.spines['top'].set_visible(False)
-     
+
     ''' Save figure '''
     plt.tight_layout()
     plt.savefig('q8.png', bbox_inches='tight')
     plt.show()
 
+
 q8_plot()
 
 ################################## Q9 #########################################
-q9_data = data.iloc[:,:8].corr()
+q9_data = data.iloc[:, :8].corr()
+
+
 def q9_plot():
     ''' Create a canvas with width 10, height 7.5 '''
     fig_obj = plt.figure(figsize=(10, 7.5))
-    sns.heatmap(q9_data,annot=True) 
+    sns.heatmap(q9_data, annot=True)
 
     ''' Save figure '''
     # plt.tight_layout()
     plt.savefig('q9.png', bbox_inches='tight')
     plt.show()
 
+
 q9_plot()
 
 # %%
-#%% <- them cai nay vao de chay interactive python giong nhu ipynb cho de code (vscode)
-# https://code.visualstudio.com/docs/python/jupyter-support-py
+# Q4
 
-# %%
+def q4_plot():
+    data.hist(bins=12, figsize=(15, 6), layout=(2, 4))
+    plt.tight_layout()
+    plt.savefig('q4.png', bbox_inches='tight')
+    plt.show()
+
+
+q4_plot()
+
+# %% <- them cai nay vao de chay interactive python giong nhu ipynb cho de code (vscode)
+# https://code.visualstudio.com/docs/python/jupyter-support-py
